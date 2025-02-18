c2e4127e3bb89cdd4fd872aec0cc3c7763a839df
halwohamda <halwohamda@gmail.com>
2025-02-17 16:02:50 -0800

    done

--- Day2 variable/app.js
+++ Day2 variable/app.js
@@ -3,19 +3,17 @@
 
 // console.log(x);
 
+// let age = 19;
 
+// let price = 20;
 
-let age = 19;
+// let gpa = 2.1;
 
-let price = 20;
+// console.log(typeof gpa);w
 
-let gpa = 2.1;
+// console.log(`my age ${age} years old`);
 
-console.log(typeof gpa);
+// console.log(`tomato price is $${price}`);
 
-console.log(`my age ${age} years old`);
-
-console.log(`tomato price is $${price}`);
-
-console.log(`gpa ${gpa}`);
+// console.log(`gpa ${gpa}`);
 
