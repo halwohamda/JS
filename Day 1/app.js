6ecdd2ef4b2a3722049359a5d6d6e8adc6262fc2
halwohamda <halwohamda@gmail.com>
2026-01-12 06:52:23 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -1,31 +1,41 @@
-// variable
+// // variable
 
-let x;
-x=23;
-console.log(x);
-
-
-// string
-
-let fullname = "hamda";
-console.log(fullname);
+// let x;
+// x=23;
+// console.log(x);
 
 
-// Boolean
+// // string
 
-let student = false;
-console.log(student);
+// let fullname = "hamda";
+// console.log(fullname);
 
 
-let firstname = "hamda adan abdi";
-let age = "20";
-let enroll = false;
+// // Boolean
 
-// console.log(`firstname ${firstname}`,
-// `her age is ${age}` ,
-//  `enroll ${enroll}`);
+// let student = false;
+// console.log(student);
 
-document.getElementById("h1").textContent = `your name is ${fullname}`;
-document.getElementById("h2").textContent = `your age is ${age} years old`;
-document.getElementById("h3").textContent = `enrolled: ${enroll}`
 
+// let firstname = "hamda adan abdi";
+// let age = "20";
+// let enroll = false;
+
+// // console.log(`firstname ${firstname}`,
+// // `her age is ${age}` ,
+// //  `enroll ${enroll}`);
+
+// document.getElementById("h1").textContent = `your name is ${fullname}`;
+// document.getElementById("h2").textContent = `your age is ${age} years old`;
+// document.getElementById("h3").textContent = `enrolled: ${enroll}`
+
+
+
+
+// arithmetic
+
+
+let students = 40;
+
+
+console.log(students)
