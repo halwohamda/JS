943795558ae675c894d589dea5f3d71595445830
halwohamda <halwohamda@gmail.com>
2026-01-13 16:01:47 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -119,16 +119,24 @@
 
 
 
-let = x;
-let = y;
-let = z;
+// let = x;
+// let = y;
+// let = z;
 
 
-x = Number(x);
-y = String(y);
-z = Boolean(z);
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
 
 
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+
+
+
+let age = window.prompt("How Old are you");
+
+console.log(age)
