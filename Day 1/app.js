79963db27673b251c673a61b61920c9d99563330
halwohamda <halwohamda@gmail.com>
2026-01-14 05:24:57 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -138,13 +138,18 @@
 // constance or const = avariable that can not be changed
 
 
-let pi = 3.14159;
-let radius;
+let PI = 3.14153;
+let radius ;
 let circumference;
 
 
 
-
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2*PI*radius;
+    document.getElementById("myH3").textContent = (circumference);
+};
 
 
 
