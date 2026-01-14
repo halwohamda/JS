9094fdb7bb34b0a3d3835b7d59b86d4e6cb4e86a
halwohamda <halwohamda@gmail.com>
2026-01-14 05:02:37 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -137,14 +137,3 @@
 
 // constance or const = avariable that can not be changed
 
-
-let pi = 3.14153;
-let radius;
-let circumference;
-
-radius = window.prompt('Enter the radius of a circumference ');
-radius = Number(radius);
-
-circumference = 2*pi*radius;
-
-console.log(circumference);
