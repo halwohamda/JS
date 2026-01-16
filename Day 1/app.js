0e440b023ad3d5384e8f8c6059f299d95221611d
halwohamda <halwohamda@gmail.com>
2026-01-16 07:55:26 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -166,3 +166,11 @@ increaseBtn.onclick = function(){
     count ++;
     countLabel.textContent = count
 };
+decreaseBtn.onclick = function(){
+    count --;
+    countLabel.textContent = count
+};
+resetBtn.onclick = function(){
+    count =0;
+    countLabel.textContent = count
+};
