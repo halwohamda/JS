99dfe63e8b0bad2b7f4781447db3fc5b2ee97df7
halwohamda <halwohamda@gmail.com>
2026-01-30 15:39:03 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -175,7 +175,7 @@
 //     countLabel.textContent = count;
 // };
 
-let x = 3.99;
+let x = 3.21;
 let y = 2;
 
 let z;
@@ -183,7 +183,8 @@ let z;
 // z = Math.round(x);
 // z = Math.floor(x);
 // z = Math.ceil(x);
-z = Math.trunc(x);
+// z = Math.trunc(x);
+z = Math.pow(x,y);
 
 console.log(z);
 
