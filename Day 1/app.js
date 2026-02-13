5c6f166e6f781dbac70ad40edcc8f7a858acdf80
halwohamda <halwohamda@gmail.com>
2026-02-13 05:01:40 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -212,22 +212,22 @@
 
 // console.log(randomNum);
 
-const Mybutton = document.getElementById("myButton");
-const Label1 = document.getElementById("Label1");
-const Label2 = document.getElementById("Label2");
-const Label3 = document.getElementById("Label3");
-const min = 1;
-const max = 6;
-let randumNum1;
-let randumNum2;
-let randumNum3;
+// const Mybutton = document.getElementById("myButton");
+// const Label1 = document.getElementById("Label1");
+// const Label2 = document.getElementById("Label2");
+// const Label3 = document.getElementById("Label3");
+// const min = 1;
+// const max = 6;
+// let randumNum1;
+// let randumNum2;
+// let randumNum3;
 
 
-Mybutton.onclick = function(){
-    randumNum1 = Math.floor(Math.random() * max) + min;
-    randumNum2 = Math.floor(Math.random() * max) + min;
-    randumNum3 = Math.floor(Math.random() * max) + min;
-    Label1.textContent = randumNum1;
-    Label2.textContent = randumNum2;
-    Label3.textContent = randumNum3;
-}
+// Mybutton.onclick = function(){
+//     randumNum1 = Math.floor(Math.random() * max) + min;
+//     randumNum2 = Math.floor(Math.random() * max) + min;
+//     randumNum3 = Math.floor(Math.random() * max) + min;
+//     Label1.textContent = randumNum1;
+//     Label2.textContent = randumNum2;
+//     Label3.textContent = randumNum3;
+// }
