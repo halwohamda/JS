d379e119f7d44f31355a02d20df05d7e21b9ccca
halwohamda <halwohamda@gmail.com>
2026-02-15 11:52:32 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -265,20 +265,20 @@
 // };
 
 
-let age = 18;
+// let age = 18;
 
-let hasLicense = true;
+// let hasLicense = true;
 
-if(age >= 16){
-    console.log("you are old enough to drive ");
+// if(age >= 16){
+//     console.log("you are old enough to drive ");
 
-if(hasLicense){
-      console.log("you have your license ");
-}else{
-    console.log("you have Not  your license Yet ");
-}
+// if(hasLicense){
+//       console.log("you have your license ");
+// }else{
+//     console.log("you have Not  your license Yet ");
+// }
 
 
-}else{
-    console.log("you are Not  old enough to drive ");
-}
+// }else{
+//     console.log("you are Not  old enough to drive ");
+// }
