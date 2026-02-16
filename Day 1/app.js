8a2fd816d7d61639abc80ec144e3c9d4c3ef900b
halwohamda <halwohamda@gmail.com>
2026-02-16 15:56:50 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -286,22 +286,30 @@
 
 const myText = document.getElementById("myText");
 const mySubmit = document.getElementById("mySubmit");
-const resultElement = document.getElementById("resultElement");
+const resultElement  = document.getElementById("resultElement ");
 
 let age;
 
 mySubmit.onclick = function(){
+
+
     age = myText.value;
     age = Number(age);
 
     if(age >=100){
         resultElement.textContent = `you are Too old to enter this site`
 
-    }else if (age == 0){
+    }
+    
+    else if(age == 0){
         resultElement.textContent = `you cant enter you were just born`
-    }else if (age >=18){
+    }
+    
+    else if(age >=18){
         resultElement.textContent = `you are old enought to enter this site`
-    }else if (age < 0){
+    }
+    
+    else if(age < 0){
         resultElement.textContent = `your age cant be below 0`
     }
 
