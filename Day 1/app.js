2ccf1c5decdbf4a3a48f39a843058fe5d53b1dda
halwohamda <halwohamda@gmail.com>
2026-02-19 17:44:42 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -318,32 +318,32 @@
 
 // .checked = property that determines the checkd state of an HTML checkbox or radio button element 
 
-const myCheckbox = document.getElementById("myCheckBox");
-const visaBtn = document.getElementById("visaBtn");
-const masterCardBtn = document.getElementById("masterCardBtn");
-const payPalBtn = document.getElementById("payPalBtn");
-const subResult = document.getElementById("subResult");
-const mySubmit = document.getElementById("mySubmit");
-const paymentResult = document.getElementById("paymentResult");
+// const myCheckbox = document.getElementById("myCheckBox");
+// const visaBtn = document.getElementById("visaBtn");
+// const masterCardBtn = document.getElementById("masterCardBtn");
+// const payPalBtn = document.getElementById("payPalBtn");
+// const subResult = document.getElementById("subResult");
+// const mySubmit = document.getElementById("mySubmit");
+// const paymentResult = document.getElementById("paymentResult");
 
 
-mySubmit.onclick = function(){
-    if(myCheckbox.checked){
-        subResult.textContent = `You are subscribed !`;
-    }
-     else{
-        subResult.textContent = `You are Not subscribed !`;
-    }
+// mySubmit.onclick = function(){
+//     if(myCheckbox.checked){
+//         subResult.textContent = `You are subscribed !`;
+//     }
+//      else{
+//         subResult.textContent = `You are Not subscribed !`;
+//     }
 
-    if(visaBtn.checked){
-        paymentResult.textContent = `you are paying with visa`
-    }
-     else if(masterCardBtn.checked){
-        paymentResult.textContent = `you are paying with mastercard`;
-    } 
-    else if (payPalBtn.checked){
-        paymentResult.textContent = `you are paying with paypal`;
-    } else{
-        paymentResult.textContent = `You must Select a payment `
-    }
-};
+//     if(visaBtn.checked){
+//         paymentResult.textContent = `you are paying with visa`
+//     }
+//      else if(masterCardBtn.checked){
+//         paymentResult.textContent = `you are paying with mastercard`;
+//     } 
+//     else if (payPalBtn.checked){
+//         paymentResult.textContent = `you are paying with paypal`;
+//     } else{
+//         paymentResult.textContent = `You must Select a payment `
+//     }
+// };
