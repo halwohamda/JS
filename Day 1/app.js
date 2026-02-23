550cca2ecc7b2647207578bb1a2ddb6631323da8
halwohamda <halwohamda@gmail.com>
2026-02-23 17:04:55 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -386,31 +386,31 @@
 // Switch = can be an efficient replacement to many else if statement 
 
 
-let day = 'pizza';
+// let day = 'pizza';
 
-switch(day){
-    case  1:
-        console.log("It is monday");
-        break;
-    case  2:
-        console.log("It is tuesday");
-        break;
-    case  3:
-        console.log("It is wednesday");
-        break;
+// switch(day){
+//     case  1:
+//         console.log("It is monday");
+//         break;
+//     case  2:
+//         console.log("It is tuesday");
+//         break;
+//     case  3:
+//         console.log("It is wednesday");
+//         break;
         
-    case  4:
-            console.log("It is thurstday");
-            break;
-    case  5:
-        console.log("It is Friday");
-        break;
-    case  6:
-            console.log("It is saturday");
-            break;
-    case  7:
-                console.log("It is sunday");
-                break;
-    default:
-        console.log(`${day} isnot a Day`);
-};
+//     case  4:
+//             console.log("It is thurstday");
+//             break;
+//     case  5:
+//         console.log("It is Friday");
+//         break;
+//     case  6:
+//             console.log("It is saturday");
+//             break;
+//     case  7:
+//                 console.log("It is sunday");
+//                 break;
+//     default:
+//         console.log(`${day} isnot a Day`);
+// };
