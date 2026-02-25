792d571e777421e20e76bb11b7bf0977c42f6fdd
halwohamda <halwohamda@gmail.com>
2026-02-25 13:48:18 -0600

    done

--- Day 1/app.js
+++ Day 1/app.js
@@ -414,25 +414,27 @@
 //     default:
 //         console.log(`${day} isnot a Day`);
 // };
-let testScore = 33;
-let letterGrade;
 
-switch(true){
-    case testScore >= 90:
-    letterGrade = "A";
-    break;
-    case testScore >= 80:
-        letterGrade = "B";
-        break;
-    case testScore >= 70:
-         letterGrade = "C";
-         break;
-    case testScore >= 60:
-         letterGrade = "D";
-        break;
-    default:
-        letterGrade = "F"
+
+// let testScore = 33;
+// let letterGrade;
+
+// switch(true){
+//     case testScore >= 90:
+//     letterGrade = "A";
+//     break;
+//     case testScore >= 80:
+//         letterGrade = "B";
+//         break;
+//     case testScore >= 70:
+//          letterGrade = "C";
+//          break;
+//     case testScore >= 60:
+//          letterGrade = "D";
+//         break;
+//     default:
+//         letterGrade = "F"
    
-}
+// }
 
-console.log(letterGrade);
+// console.log(letterGrade);
